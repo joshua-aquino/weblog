dbd5841df8cdd8f9f8c50cd2bddad6e430df3d0b
Joshua Aquino <email@joshuaaquino.xyz>
2022-03-01 14:22:08 -0500

    Add template support

--- weblog.go
+++ weblog.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-  "fmt"
+//  "fmt"
   "net/http"
+  "html/template"
 )
 
+type BlogPage struct {
+  Title string
+  Blog string
+}
+
+/*
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>suck it bob</h1>")
 }
+*/
+
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+  p := BlogPage{Title: "bob", Blog: "sucks ass"}
+  t, _ := template.ParseFiles("template.html")
+  t.Execute(w, p)
+}
 
 func main() {
-  http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/", blogHandler)
   http.ListenAndServe(":8000", nil)
 }
