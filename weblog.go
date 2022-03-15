c3d7f4f111c1f5070cfef5f6a6377bc605b0d6e2
Joshua Aquino <email@joshuaaquino.xyz>
2022-03-15 15:59:44 -0400

    experiment with nested ranges

--- weblog.go
+++ weblog.go
@@ -1,14 +1,25 @@
 package main
+// once this runs, it should already have the page consructed in data
+// only one execution should be necessary
 
 import (
-//  "fmt"
   "net/http"
   "html/template"
+  "os"
+  "log"
+  "fmt"
 )
 
+/*
 type BlogPage struct {
   Title string
   Blog string
+}
+*/
+
+type BlogHome struct {
+  Years []string
+  Blogs []string
 }
 
 /*
@@ -18,12 +29,38 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 */
 
 func blogHandler(w http.ResponseWriter, r *http.Request) {
-  p := BlogPage{Title: "bob", Blog: "sucks ass"}
-  t, _ := template.ParseFiles("template.html")
+  var blogYears = []string{}
+  var blogs = []string{}
+  files, err := os.ReadDir("./blog")
+  if err != nil {
+      log.Fatal(err)
+  }
+  for _, f := range files {
+    blogYears = append(blogYears, f.Name())
+    giles, err := os.ReadDir("./blog/" + f.Name())
+    if err != nil {
+        log.Fatal(err)
+    }
+    for _, g := range giles {
+      blogs = append(blogs, g.Name())
+      fmt.Println(blogs)
+      fmt.Fprintf(w, blogs)
+    }
+    blogs = nil
+  }
+
+  p := BlogHome{Years: blogYears, Blogs: blogs}
+  t, _ := template.ParseFiles("blog.html")
   t.Execute(w, p)
+/*
+  filename := "carl.html"
+  body, _ := template.ParseFiles(filename)
+  body.Execute(w, p)
+*/
 }
 
 func main() {
   http.HandleFunc("/", blogHandler)
   http.ListenAndServe(":8000", nil)
 }
+
